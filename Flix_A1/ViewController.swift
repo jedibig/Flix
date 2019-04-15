d77009c613cf6122ed2cc59d5f7072e0eb004f56
Jedidiah Bowo <jbowo@uci.edu>
2019-04-14 22:16:10 -0700

    Initial Commit

---
+++ Flix_A1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix_A1
+//
+//  Created by Kay Lab on 4/14/19.
+//  Copyright © 2019 JB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
